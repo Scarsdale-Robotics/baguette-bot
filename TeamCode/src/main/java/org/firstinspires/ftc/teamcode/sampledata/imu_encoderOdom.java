c2a87623bfa8896f9f71021c8e02c0768d594b70
Arjan <bawejaa35@gmail.com>
2025-05-12 23:04:50 -0400

    IMU and One encoder System

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sampledata/imu_encoderOdom.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.sampledata;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class imu_encoderOdom {
+    private final DcMotor centerEncoder;
+    private final IMU imu;
+
+    private double x;
+    private double y;
+    private double heading;
+
+    private double d;
+
+    private double lastx;
+    private double lasty;
+    private double lastheading;
+    private double lastd;
+
+    public imu_encoderOdom(HardwareMap hm) {
+        centerEncoder = hm.get(DcMotor.class, "centerEncoder");
+        imu = hm.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.RIGHT;
+
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.resetYaw();
+    }
+
+    public void update(){
+        d = centerEncoder.getCurrentPosition();
+        double deltaD = (d - lastd);
+        lastd = d;
+
+        double headingRad = getCurHeading();
+        //im not rlly sure if i need deltaHeading cus i dont use it
+        double deltaHeading = headingRad - lastheading;
+        lastheading = headingRad;
+
+        double TICKS_PER_INCH = 1000; // update i forgor
+        double deltaDistanceInInches = deltaD / TICKS_PER_INCH;
+
+        x += deltaDistanceInInches * Math.cos(headingRad);
+        y += deltaDistanceInInches * Math.sin(headingRad);
+    }
+
+
+    public double getCurHeading(){
+        double rawHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        heading = rawHeading + Math.PI/2;
+        //heading i think should only be between 0 and pi
+        heading = Math.atan2(Math.sin(heading), Math.cos(heading));
+        return heading;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+}
