48a3f7a6cfe405b57fa220ebc914e965204eb0c9
Arjan <bawejaa35@gmail.com>
2025-05-16 16:57:57 -0400

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sampledata/MoveOdom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sampledata/MoveOdom.java
@@ -3,19 +3,32 @@ package org.firstinspires.ftc.teamcode.sampledata;
 import static org.firstinspires.ftc.teamcode.MainTeleop.LEFT_POWER_FACTOR;
 import static org.firstinspires.ftc.teamcode.MainTeleop.RIGHT_POWER_FACTOR;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.teamcode.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.Motor;
 import org.firstinspires.ftc.teamcode.scaleOperators.Clamp;
 import org.firstinspires.ftc.teamcode.scaleOperators.Rescale;
 
+import com.qualcomm.hardware.limelightvision.LLResultTypes.FiducialResult;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.List;
+@TeleOp(name = "xxyz")
 public class MoveOdom extends LinearOpMode {
 
     imu_encoderOdom odom;
     DriveSubsystem drive;
+    Limelight3A limelight;
 
-    @Override
+    double[] apriltagvals;
+    // 0 = x // 1 = y // 2  = heading
+
+
     public void runOpMode() throws InterruptedException {
         odom =  new imu_encoderOdom(hardwareMap, 0,0,0);
         Motor left = new Motor.Builder(hardwareMap, "left")
@@ -32,18 +45,40 @@ public class MoveOdom extends LinearOpMode {
                 .invert()
                 .build();
 
-
-
-
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100);
+        limelight.start();
         drive = new DriveSubsystem(left, right);
+        apriltagvals = new double[3];
+
+        waitForStart();
+
         while (opModeIsActive()){
             odom.update();
+            apriltagUpdate();
             double leftPower = gamepad1.left_stick_y * LEFT_POWER_FACTOR;
             double rightPower = gamepad1.right_stick_y * RIGHT_POWER_FACTOR;
             drive.motorsByPowers(leftPower, rightPower);
-            telemetry.addLine("x: " + odom.getX());
-            telemetry.addLine("y: " + odom.getY());
-            telemetry.addLine("Heading " + odom.getCurHeading());
+            telemetry.addData("Odom", 0);
+            telemetry.addData("x", odom.getX());
+            telemetry.addData("y", odom.getY());
+            telemetry.addData("Heading", odom.getCurHeading());
+            telemetry.addData("AprilTag", 0);
+            telemetry.addData("x", apriltagvals[0]);
+            telemetry.addData("y", apriltagvals[1]);
+            telemetry.update();
+        }
+
+
+    }
+    public void apriltagUpdate(){
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            List<FiducialResult> tags = result.getFiducialResults();
+            if (!tags.isEmpty()) {
+                apriltagvals[0] = tags.get(0).getTargetPoseRobotSpace().getPosition().x;
+                apriltagvals[1] = tags.get(0).getTargetPoseRobotSpace().getPosition().y;
+            }
         }
     }
 }
